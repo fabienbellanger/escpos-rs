2491752ab3e522c545120b2714f5d7a531b7f3d8
Fabien Bellanger <fabien.bellanger@apitic.com>
2024-02-21 15:03:45 +0100

    First version with page code management

--- src/domain/protocol.rs
+++ src/domain/protocol.rs
@@ -4,6 +4,7 @@
 use super::bit_image::*;
 use super::codes::*;
 use super::{character::*, common::get_parameters_number_2, constants::*, types::*};
+use crate::domain::page_codes::PageCodeTable;
 use crate::{
     errors::{PrinterError, Result},
     io::encoder::Encoder,
@@ -176,8 +177,37 @@ impl Protocol {
     }
 
     /// Print text
-    pub(crate) fn text(&self, text: &str) -> Result<Command> {
-        self.encoder.encode(text)
+    // TODO Complete tests with Page code
+    pub(crate) fn text(&self, text: &str, page_code: Option<PageCode>) -> Result<Command> {
+        if let Some(page_code) = page_code {
+            let table: PageCodeTable = page_code.try_into()?;
+            let table = table.get_table();
+
+            let mut cmd = Vec::new();
+            let mut current = String::new();
+
+            for c in text.chars() {
+                if let Some(&n) = table.get(&c) {
+                    if !current.is_empty() {
+                        cmd.append(&mut self.encoder.encode(&current)?);
+                        current.clear();
+                    }
+
+                    cmd.push(n);
+                } else {
+                    current.push(c);
+                }
+            }
+
+            if !current.is_empty() {
+                cmd.append(&mut self.encoder.encode(&current)?);
+                current.clear();
+            }
+
+            Ok(cmd)
+        } else {
+            self.encoder.encode(text)
+        }
     }
 
     /// Set horizontal and vertical motion units
@@ -808,7 +838,7 @@ mod tests {
     #[test]
     fn test_text() {
         let protocol = Protocol::new(Encoder::default());
-        assert_eq!(protocol.text("My text").unwrap(), "My text".as_bytes());
+        assert_eq!(protocol.text("My text", None).unwrap(), "My text".as_bytes());
     }
 
     #[test]
